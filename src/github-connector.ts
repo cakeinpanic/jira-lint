8a166750fb98d508df9f01a82bbc32591207f273
Katya Pavlenko <cakeinpanic@gmail.com>
2020-06-26 20:38:22 +0300

    --amend

--- src/github-connector.ts
+++ src/github-connector.ts
@@ -1,64 +1,77 @@
-import { getInputs } from './inputs'
-import { JIRADetails, PullRequestParams } from './types'
-import { PullsUpdateParams } from '@octokit/rest'
-import { getPRDescription, shouldUpdatePRDescription } from './utils'
-import * as github from '@actions/github'
-import * as core from '@actions/core'
+import { getInputs } from './inputs';
+import { JIRADetails, PullRequestParams } from './types';
+import { PullsUpdateParams } from '@octokit/rest';
+import { getJIRAIssueKey, getJIRAIssueKeysByCustomRegexp, getPRDescription, shouldUpdatePRDescription } from './utils';
+import * as github from '@actions/github';
+import * as core from '@actions/core';
 
 /** Update a PR details. */
 export const updatePrDetails = async (client: github.GitHub, prData: PullsUpdateParams): Promise<void> => {
   try {
-    await client.pulls.update(prData)
+    await client.pulls.update(prData);
   } catch (error) {
-    core.setFailed(error.message)
-    process.exit(1)
+    core.setFailed(error.message);
+    process.exit(1);
   }
-}
+};
 
 export class GithubConnector {
-  client: github.GitHub
+  client: github.GitHub;
 
   constructor() {
-    const { GITHUB_TOKEN, } = getInputs()
+    const { GITHUB_TOKEN } = getInputs();
 
-    this.client = new github.GitHub(GITHUB_TOKEN)
+    this.client = new github.GitHub(GITHUB_TOKEN);
   }
 
   get headBranch(): string {
-    const { pullRequest } = getGithubData()
+    const { pullRequest } = getGithubData();
     const {
       head: { ref: headBranch },
-    } = pullRequest
+    } = pullRequest;
 
-    return headBranch
+    return headBranch;
   }
 
-  async updatePrDetails(details: JIRADetails) {
-    const { repository, owner, pullRequest } = getGithubData()
-    const { name: repo } = repository
+  getIssueKeyFromTitle(): string | null {
+    const { pullRequest } = getGithubData();
+    const { JIRA_PROJECT_KEY, CUSTOM_ISSUE_NUMBER_REGEXP } = getInputs();
 
-    const {
-      number: prNumber = 0,
-      body: prBody = '',
-    } = pullRequest
+    const shouldUseCustomRegexp = !!CUSTOM_ISSUE_NUMBER_REGEXP && !!JIRA_PROJECT_KEY;
+    const PRTitle = pullRequest.title;
+
+    console.log(PRTitle);
+
+    if (!PRTitle) {
+      console.log(`JIRA issue id is missing in your PR title ${PRTitle}, doing nothing`);
+      return null;
+    }
+    return shouldUseCustomRegexp
+      ? getJIRAIssueKeysByCustomRegexp(PRTitle, CUSTOM_ISSUE_NUMBER_REGEXP, JIRA_PROJECT_KEY)
+      : getJIRAIssueKey(PRTitle);
+  }
+  async updatePrDetails(details: JIRADetails) {
+    const { repository, owner, pullRequest } = getGithubData();
+    const { name: repo } = repository;
+
+    const { number: prNumber = 0, body: prBody = '' } = pullRequest;
     if (!shouldUpdatePRDescription(prBody)) {
-      return
+      return;
     }
     const prData: PullsUpdateParams = {
       owner,
       repo,
       pull_number: prNumber,
       body: getPRDescription(prBody, details),
-    }
-    return await updatePrDetails(this.client, prData)
+    };
+    return await updatePrDetails(this.client, prData);
   }
-
 }
 
 export interface IGithubData {
-  repository: any,
-  owner: any,
-  pullRequest: PullRequestParams
+  repository: any;
+  owner: any;
+  pullRequest: PullRequestParams;
 }
 
 export const getGithubData = (): IGithubData => {
@@ -68,10 +81,10 @@ export const getGithubData = (): IGithubData => {
       organization: { login: owner },
       pull_request: pullRequest,
     },
-  } = github.context
+  } = github.context;
   return {
     repository,
     owner,
-    pullRequest: pullRequest as PullRequestParams
-  }
-}
+    pullRequest: pullRequest as PullRequestParams,
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,30 @@
-import * as core from '@actions/core'
-import { getJIRAIssueKey, getJIRAIssueKeysByCustomRegexp, shouldSkipBranchLint, } from './utils'
-import { getInputs } from './inputs'
-import { GithubConnector } from './github-connector'
-import { JiraConnector } from './jira-connector'
+import * as core from '@actions/core';
+import { shouldSkipBranchLint } from './utils';
+import { getInputs } from './inputs';
+import { GithubConnector } from './github-connector';
+import { JiraConnector } from './jira-connector';
 
 async function run(): Promise<void> {
   try {
     const { BRANCH_IGNORE_PATTERN } = getInputs();
 
-    const githubConnector = new GithubConnector()
-    const jiraConnector = new JiraConnector()
+    const githubConnector = new GithubConnector();
+    const jiraConnector = new JiraConnector();
 
     if (shouldSkipBranchLint(githubConnector.headBranch, BRANCH_IGNORE_PATTERN)) {
       process.exit(0);
     }
 
-    const issueKey = getIssueKey(githubConnector.headBranch)
+    const issueKey = githubConnector.getIssueKeyFromTitle();
 
     if (!issueKey) {
-      console.log(`JIRA issue id is missing in your branch ${githubConnector.headBranch}, doing nothing`)
-      process.exit(1);
+      process.exit(0);
     }
 
     console.log(`JIRA key -> ${issueKey}`);
 
-    const details = await jiraConnector.getTicketDetails(issueKey)
-    await githubConnector.updatePrDetails(details)
-
-
+    const details = await jiraConnector.getTicketDetails(issueKey);
+    await githubConnector.updatePrDetails(details);
   } catch (error) {
     console.log({ error });
     core.setFailed(error.message);
@@ -35,13 +32,4 @@ async function run(): Promise<void> {
   }
 }
 
-const getIssueKey = (headBranch: string): string | null => {
-  const { JIRA_PROJECT_KEY, CUSTOM_ISSUE_NUMBER_REGEXP } = getInputs()
-
-  const shouldUseCustomRegexp = !!CUSTOM_ISSUE_NUMBER_REGEXP && !!JIRA_PROJECT_KEY
-
-  return shouldUseCustomRegexp
-    ? getJIRAIssueKeysByCustomRegexp(headBranch, CUSTOM_ISSUE_NUMBER_REGEXP, JIRA_PROJECT_KEY)
-    : getJIRAIssueKey(headBranch)
-}
 run();
